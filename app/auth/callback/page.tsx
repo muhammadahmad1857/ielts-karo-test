1ea7a0f2a103c4f01a80540099b3f5b42dc60b20
= <ahmedjawad1857@gmail.com>
2025-10-30 23:30:39 +0500

    fix: refactor AuthCallbackPage to use async/await and remove unused code

--- app/auth/callback/page.tsx
+++ app/auth/callback/page.tsx
@@ -1,38 +1,67 @@
-"use client";
+// "use client";
 
-import { useEffect } from "react";
-import { useSearchParams, useRouter } from "next/navigation";
-import { api } from "@/lib/axios";
-import Spinner from "@/components/Loader";
+// import { useEffect } from "react";
+// import { useSearchParams, useRouter } from "next/navigation";
+// import { api } from "@/lib/axios";
+// import Spinner from "@/components/Loader";
+// import { handleGoogleCallback } from "@/dal";
+// import { setToken } from "@/dal/auth/token-storage";
+// export const dynamic = "force-dynamic";
+
+// export default function CallbackPage() {
+//   const searchParams = useSearchParams();
+//   const router = useRouter();
+
+//   useEffect(() => {
+//     const code = searchParams.get("code");
+//     if (!code) return;
+
+//     const fetchToken = async () => {
+//       const res = await handleGoogleCallback({ code });
+//       if (res.success && res.data?.access_token) {
+//         setToken("token", res.data?.access_token);
+//         router.push("/admin");
+//       } else {
+//         console.error(res.error);
+//       }
+//     };
+
+//     fetchToken();
+//   }, [searchParams, router]);
+
+//   return (
+//     <div className="flex flex-col gap-4">
+//       <Spinner />
+//       Logging in...
+//     </div>
+//   );
+// }
+
 import { handleGoogleCallback } from "@/dal";
-import { setToken } from "@/dal/auth/token-storage";
+import { redirect } from "next/navigation";
+
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
-export default function CallbackPage() {
-  const searchParams = useSearchParams();
-  const router = useRouter();
+export default async function AuthCallbackPage({
+  searchParams,
+}: {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  searchParams: any;
+}) {
+  try {
+    const code = searchParams.code;
+    if (!code) throw new Error("Missing code");
 
-  useEffect(() => {
-    const code = searchParams.get("code");
-    if (!code) return;
+    // Exchange the Google auth code for tokens
+    const res = await handleGoogleCallback({ code });
+    if (!res.success) throw new Error("Failed to authenticate");
 
-    const fetchToken = async () => {
-      const res = await handleGoogleCallback({ code });
-      if (res.success && res.data?.access_token) {
-        setToken("token", res.data?.access_token);
-        router.push("/admin");
-      } else {
-        console.error(res.error);
-      }
-    };
-
-    fetchToken();
-  }, [searchParams, router]);
-
-  return (
-    <div className="flex flex-col gap-4">
-      <Spinner />
-      Logging in...
-    </div>
-  );
+    // maybe redirect to dashboard after success
+    redirect("/");
+  } catch (e) {
+    console.error("OAuth callback failed:", e);
+    redirect("/login?error=auth_failed");
+  }
+  
 }
